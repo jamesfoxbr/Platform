f94591a5dd26906d309650fd4c6ed466626fe242
Márcio Machado Ribeiro <jamesfoxbr@hotmail.com>
2024-02-27 18:42:33 -0300

    Starting collisions

--- Platformer.h
+++ Platformer.h
@@ -6,15 +6,19 @@
 
 #include "Player.h"
 
+#include <iostream>
 #include <algorithm>
 #include <vector>
 
 // Global Variables and Constants
-constexpr unsigned int  TILE_WIDTH = 8;
-constexpr unsigned int  TILE_HEIGHT = 8;
+constexpr unsigned int  TILE_WIDTH   = 8;
+constexpr unsigned int  TILE_HEIGHT  = 8;
 
-constexpr unsigned int SCREEN_WIDTH = 128;
+constexpr unsigned int SCREEN_WIDTH  = 128;
 constexpr unsigned int SCREEN_HEIGHT = 128;
+
+constexpr unsigned int STAGE_WIDTH   = 16;
+constexpr unsigned int STAGE_HEIGHT  = 16;
 
 //////////////////////////////////////////////////////////////////////////
 //                        PLATFORM CLASS                                //
@@ -42,9 +46,9 @@ public:
 
 		DrawScreen();
 
-		PlayerFunctions(fElapsedTime);
-
 		DrawPLayer();
+
+		PlayerFunctions(fElapsedTime);
 
 		return true;
 	}
@@ -57,14 +61,11 @@ public:
 
 	void MakeStage()
 	{
-		int stageWidth  = stage.size();
-		int stageWeight = stage[0].size();
-
-		for (int y = 0; y < stageWeight; y++)
+		for (int y = 0; y < STAGE_HEIGHT; y++)
 		{
-			for (int x = 0; x < stageWidth; x++)
+			for (int x = 0; x < STAGE_WIDTH; x++)
 			{
-				if (stage[x][y] == 1)
+				if (stage[y][x] == 1)
 					FillRect(x * TILE_WIDTH, y * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT, olc::DARK_YELLOW);
 			}
 		}
@@ -74,23 +75,35 @@ public:
 	{
 		MovePlayer(fElapsedTime);
 		Dessacelerate(fElapsedTime);
-
+		Collision(7, 0, 1, 0);
 	}
 
 	void MovePlayer(float fElapsedTime)
 	{
 		if (GetKey(olc::Key::RIGHT).bHeld) player.dx += player.acceleration;
 		if (GetKey(olc::Key::LEFT).bHeld)  player.dx -= player.acceleration;
+		if (GetKey(olc::Key::DOWN).bHeld)  player.dy += player.acceleration;
+		if (GetKey(olc::Key::UP).bHeld)    player.dy -= player.acceleration;
+
+		ClampPLayerToScreen();
 
 		// Max speed
 		player.dx = std::clamp(player.dx, -50.0f, 50.0f);
+		player.dy = std::clamp(player.dy, -50.0f, 50.0f);
 
 		player.x += player.dx * fElapsedTime;
+		player.y += player.dy * fElapsedTime;
+	}
+
+	void ClampPLayerToScreen()
+	{
+		player.x = std::clamp(player.x, 0.0f, 127.0f - TILE_WIDTH);
 	}
 
 	void Dessacelerate(float fElapsedTime)
 	{
 		if (player.dx != 0) player.dx *= player.dessaceleration;
+		if (player.dy != 0) player.dy *= player.dessaceleration;
 
 	}
 
@@ -120,6 +133,20 @@ private:
 	float fTargetFrameTime = 1.0f / 100.0f; // Virtual FPS of 100fps
 	float fAccumulatedTime = 0.0f;
 
+	// Collisions
+	void Collision(float adjustX, float adjustY, float moveToX, float moveToY)
+	{
+		int playerToMapPosX = (player.x / 8) + adjustX / 8;
+		int playerToMapPosY = (player.y / 8) + adjustY / 8;
+
+		FillRect(player.x + adjustX, player.y + adjustY, 1, 1, olc::RED);
+
+		if (stage[playerToMapPosY][playerToMapPosX] == 1)
+		{
+			player.x += 1.0f;
+		}
+	}
+
 	// Crete stage
 	std::vector<std::vector<int>> stage =
 	{
@@ -129,14 +156,14 @@ private:
 		{0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, // 4
 		{0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, // 5
 		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, // 6
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, // 7
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, // 8
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, // 9
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, // 10
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, // 11
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, // 12
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, // 13
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, // 14
+		{1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, // 7
+		{1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, // 8
+		{1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, // 9
+		{1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, // 10
+		{1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, // 11
+		{1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, // 12
+		{1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, // 13
+		{1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, // 14
 		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, // 15
 		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, // 16
 	};
